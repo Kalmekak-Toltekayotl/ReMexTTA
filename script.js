22561a1874bb15efd66b422d5e2e3c4f77f85dd0
Kalmekak-Toltekayotl <kalmekaktoltekayotl@gmail.com>
2025-11-07 20:49:46 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+function buscar() {
+  const query = document.getElementById("query").value.trim();
+  const resultados = document.getElementById("resultados");
+
+  if (!query) {
+    resultados.innerHTML = "<p>Por favor, escribe un nombre o clave.</p>";
+    return;
+  }
+
+  resultados.innerHTML = "<p>Buscando resultados para: <b>" + query + "</b>...</p>";
+
+  // Simulación temporal
+  setTimeout(() => {
+    resultados.innerHTML = `<p>No se encontraron resultados para "<b>${query}</b>".</p>`;
+  }, 1200);
+}
